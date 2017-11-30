38f286022bce729c6c2de42c9664eb5a8ed54b1c
Sergey Marin <marin.sergey@gmail.com>
2017-11-30 23:56:18 +0200

    Support for gherkin's TableNode

--- src/Transformer/PlaceholderArgumentTransformer.php
+++ src/Transformer/PlaceholderArgumentTransformer.php
@@ -7,6 +7,7 @@ use Behat\Behat\Definition\Call\DefinitionCall;
 use Behat\Behat\Transformation\Transformer\ArgumentTransformer;
 use espend\Behat\PlaceholderExtension\PlaceholderBagInterface;
 use espend\Behat\PlaceholderExtension\Utils\PlaceholderUtil;
+use Behat\Gherkin\Node\TableNode;
 
 /**
  * @author Daniel Espendiller <daniel@espendiller.net>
@@ -31,11 +32,50 @@ class PlaceholderArgumentTransformer implements ArgumentTransformer
      */
     public function supportsDefinitionAndArgument(DefinitionCall $definitionCall, $argumentIndex, $argumentValue)
     {
-        if (!is_string($argumentValue)) {
-            return false;
+        if (is_string($argumentValue)) {
+            // '%FOO%', '%foo%'
+            return $this->isStringContainPlaceholder($argumentValue);
+        } elseif ($argumentValue instanceof TableNode) {
+            foreach ($argumentValue as $item) {
+                if ($this->isStringContainPlaceholder($item['key'])) {
+                    return true;
+                }
+                if ($this->isStringContainPlaceholder($item['value'])) {
+                    return true;
+                }
+            }
         }
 
-        // '%FOO%', '%foo%'
+        return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function transformArgument(DefinitionCall $definitionCall, $argumentIndex, $argumentValue)
+    {
+
+        if (is_string($argumentValue)) {
+            // '%FOO%', '%foo%'
+            $argumentValue = $this->replaceInString($argumentValue);
+        } elseif ($argumentValue instanceof TableNode) {
+            $table = $argumentValue->getTable();
+
+            foreach ($table as $index => $item) {
+                $table[$index][0] = $this->replaceInString($item[0]);
+                $table[$index][1] = $this->replaceInString($item[1]);
+            }
+            $argumentValue = new TableNode($table);
+        }
+
+        return $argumentValue;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    private function isStringContainPlaceholder($argumentValue)
+    {
         if (PlaceholderUtil::isValidPlaceholder($argumentValue)) {
             return ($placeholders = $this->placeholderBag->all())
                 && isset($placeholders[$argumentValue]);
@@ -54,7 +94,7 @@ class PlaceholderArgumentTransformer implements ArgumentTransformer
     /**
      * {@inheritdoc}
      */
-    public function transformArgument(DefinitionCall $definitionCall, $argumentIndex, $argumentValue)
+    private function replaceInString($argumentValue)
     {
         // 'foobar%FOO%'
         foreach ($this->placeholderBag->all() as $key => $value) {
